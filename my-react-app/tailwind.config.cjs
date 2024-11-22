d725d82ec9c1767023743c58509c459d826aa613
salman <shamsisalman81@gmail.com>
2024-11-22 16:13:00 +0500

    starter

---
+++ my-react-app/tailwind.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+      ],      
+  };
+    
